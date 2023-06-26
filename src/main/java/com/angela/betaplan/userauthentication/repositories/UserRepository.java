cfb5e32b7ba0f8fafa49b872462431e68aa9603a
Angela Shegani <angelashegani@Angelas-MacBook-Air.local>
2023-06-26 13:07:00 +0200

    done

---
+++ src/main/java/com/angela/betaplan/userauthentication/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.angela.betaplan.userauthentication.repositories;
+
+
+import com.angela.betaplan.userauthentication.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
